705ee1de29b57afc66b5c3346145fec539b0dd91
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-12-13 00:04:08 +0200

    start task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,35 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+const form = document.querySelector('form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+const btn = document.querySelector('button');
+const amount = amountInput.value;
+form.addEventListener('submmit', handleSubmit);
+
+function handleSubmit (event) {
+  event.preventDefault;
+ //for (let i = 0; i < amount; i++) {
+  //createPromise;
+  console.log(amount);
+  
+ //}
+  
+ }
+
+
+
+
+
+
+
+
+//createPromise(2, 1500)
+//  .then(({ position, delay }) => {
+//    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//  })
+//  .catch(({ position, delay }) => {
+//    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//  });
